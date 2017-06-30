1c85f82c65bafc0dceadb6cc82220742983da77b
Anil Chaudhari <sunamjohn@gmail.com>
2017-06-30 14:18:04 +0545

    auth added for all methods

--- api.py
+++ api.py
@@ -31,7 +31,7 @@ def auth_func(*args, **kwargs):
     return True
 
 
-manager = APIManager(app, flask_sqlalchemy_db=db, preprocessors=dict(GET_MANY=[auth_func], POST_MANY=[auth_func]))
+manager = APIManager(app, flask_sqlalchemy_db=db, preprocessors=dict(GET_MANY=[auth_func], POST_MANY=[auth_func], GET_SINGLE=[auth_func], DELETE_SINGLE=[auth_func], DELETE_MANY=[auth_func], POST=[auth_func]))
 # manager = APIManager(app, flask_sqlalchemy_db=db)
 manager.create_api(User, methods=['GET'], results_per_page=0)
 manager.create_api(Branch, methods=['GET', 'POST', 'DELETE', 'PATCH'])
